a22cc51433fa48f7ad011556c95072eb544a7ada
Maksimchuk Valentin <37192188+MaximchukVal@users.noreply.github.com>
2020-10-27 18:15:20 +0300

    Added lab 7_1

---
+++ parallel_programming_labs/lab_7_1/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <omp.h>
+#include <cmath>
+#include <ctime>
+
+#define THREAD_NUM 4
+
+void printArray(double array[], int len)
+{
+  for (int i = 0; i < len; i++)
+  {
+    std::cout << array[i] << std::endl;
+  }
+}
+
+int main(int argc, char** argv)
+{
+  omp_set_num_threads(THREAD_NUM);
+  const int N = 100000;
+  double a[N], b[N];
+  a[0] = 1;
+  b[0] = 1;
+  double x;
+  double result = 0;
+  int i = 0;
+  unsigned int t_start = 0;
+
+  t_start = clock();
+  for (int i = 1; i < N; i++)
+  {
+    x = sqrt(i);
+    result = x * (i + x*2 + 1);
+    b[i] = result;
+  }
+  std::cout << clock() - t_start << std::endl;
+  //printArray(b, N);
+
+  t_start = clock();
+#pragma omp parallel for private(i, x) shared(a) reduction(+:result)
+  for (i = 1; i < N; i++)
+  {
+    x = sqrt(i);
+    result = x * x * (x + 2) + x;
+    a[i] = result;
+  }
+  std::cout << clock() - t_start << std::endl;
+
+  //printArray(a, N);
+  return 0;
+}
